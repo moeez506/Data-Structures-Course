1186be63ea35601439b50cdf9b679fa6a296969f
Abdul Mannan <abdulmann1234@gmail.com>
2024-06-23 03:42:49 +0500

    implementation of queue using arrays

---
+++ Week 7 - Queue/concept-of-circular-queue.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+using namespace std;
+
+class Queue
+{
+private:
+    int arr[5];
+    int front = -1, rear = -1;
+    int capacity = sizeof(arr) / sizeof(arr[0]);
+
+public:
+    /** isFull()
+     * @return true if the queue is full
+     */
+    bool isFull()
+    {
+        if ((rear + 1) % capacity == front)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /** isEmpty
+     * @return true if the queue is empty,
+     */
+    bool isEmpty()
+    {
+        if (front == -1 && rear == -1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /** Enqueue
+     * @return data {to input data into the queue at rear position}
+     */
+    void Enqueue(int data)
+    {
+        if (isFull())
+        {
+            cout << "Queue is full!" << endl;
+            return;
+        }
+        else if (isEmpty())
+        {
+            front = 0;
+            rear = 0;
+        }
+        else
+        {
+            rear = (rear + 1) % capacity;
+        }
+        arr[rear] = data;
+        cout << "Enqueued " << arr[rear] << endl;
+    }
+
+    /** Dequeue
+     * @return it returns the data which is being removed from the queue
+     */
+    int dequeue()
+    {
+        if (isEmpty())
+        {
+            cout << "Queue is empty!" << endl;
+            return -1;
+        }
+        int temp = arr[front];
+        if (front == rear)
+        {
+            front = -1;
+            rear = -1;
+        }
+        else
+        {
+            front = (front + 1) % capacity;
+        }
+
+        cout << "Dequeueing..." << temp << endl;
+        return temp;
+    }
+
+    /** peek
+     * @return the value at the front of the queue
+     */
+    int peek()
+    {
+        if (isEmpty())
+        {
+            cout << "Queue is empty!" << endl;
+            return -1;
+        }
+        return arr[front];
+    }
+};
+
+int main()
+{
+    Queue q;
+    q.Enqueue(2);
+    q.Enqueue(3);
+    q.Enqueue(4);
+    q.Enqueue(6);
+    q.Enqueue(9);
+    q.Enqueue(10);
+    q.Enqueue(15);
+    cout << q.peek() << endl;
+    q.dequeue();
+    cout << q.peek() << endl;
+    return 0;
+}
